a50f45a093c715f22248544314eaeffa7df54056
Rahul <111085537+TechGiron@users.noreply.github.com>
2023-06-23 10:11:08 +0530

    Update query.py

--- plugins/query.py
+++ plugins/query.py
@@ -620,7 +620,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         )
     elif query.data == "source":
         buttons = [[
-            InlineKeyboardButton('SOURCE CODE', url='https://github.com/MrMKN/PROFESSOR-BOT')
+            InlineKeyboardButton('Channel', url='https://t.me/Botz_Guardian_Update')
             ],[
             InlineKeyboardButton('👩‍🦯 Back', callback_data='about')
         ]]
