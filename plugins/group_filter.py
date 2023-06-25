a30622886c836bf381a9af48922054ec52e39195
Rahul <111085537+TechGiron@users.noreply.github.com>
2023-06-25 12:24:55 +0530

    Update group_filter.py

--- plugins/group_filter.py
+++ plugins/group_filter.py
@@ -207,10 +207,10 @@ async def auto_filter(client, msg, spoll=False):
         else:
             return
     else:
-        sts = await message.reply('searching...')
         settings = await get_settings(msg.message.chat.id)
         message = msg.message.reply_to_message  # msg will be callback query
         search, files, offset, total_results = spoll
+    sts = await message.reply('searching...')
     pre = 'filep' if settings['file_secure'] else 'file'
     req = message.from_user.id if message.from_user else 0
 
